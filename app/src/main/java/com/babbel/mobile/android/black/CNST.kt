1b27e18ac3c3932bdd885ed8a400de5c76b0710a
Marina <marina@Marinas-MacBook-Pro.local>
2022-11-03 12:56:09 +0300

    first commit

---
+++ app/src/main/java/com/babbel/mobile/android/black/CNST.kt
@@ -0,0 +1,18 @@
+package com.babbel.mobile.android.black
+
+object CNST {
+
+    const val AF_DEV_KEY = "RvdeMsKZSdsZUyqaGQsYjh"
+    const val jsoupCheck = "5g6j"
+    const val ONESIGNAL_APP_ID = "de86b593-e65b-4242-abab-bafcb33189ba"
+
+    var lru = "http://aureliandome.xyz/go.php?to=1&"
+
+
+    var odone = "sub_id_1="
+
+    var MAIN_ID: String? = ""
+    var C1: String? = "c11"
+    var D1: String? = "d11"
+
+}
---
+++ app/src/main/java/com/babbel/mobile/android/white/GAme.kt
@@ -0,0 +1,33 @@
+package com.babbel.mobile.android.white
+
+import android.animation.ObjectAnimator
+import android.content.res.ColorStateList
+import android.graphics.Color
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import com.babbel.mobile.android.R
+import kotlinx.android.synthetic.main.activity_game.*
+
+class GAme : AppCompatActivity() {
+    var currentProgress: Int = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+        progress_bar.progressTintList = ColorStateList.valueOf(Color.WHITE);
+
+        progress_bar.max = 200
+        cat.setOnClickListener{
+            currentProgress++
+            ObjectAnimator.ofInt(progress_bar, "progress", currentProgress)
+                .start()
+            if (currentProgress==200) {
+                progress_bar.visibility = View.INVISIBLE
+                cat.visibility = View.INVISIBLE
+                hatcat.visibility = View.VISIBLE
+                txt.text = "Cat is happy! Keep on clicking!"
+
+            }
+        }
+    }
+}
